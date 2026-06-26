9f313d1bd30a1d52fa40722c20a97f43da1129f1
divyanshigupta03 <divyanshigupta1103@gmail.com>
2026-06-26 12:53:18 +0530

    Time: 18 ms (7.98%) | Memory: 70.2 MB (6.11%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,18 +9,14 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(p.val == root.val || q.val == root.val) return root;
-        if(isExists(root.left, p.val) && isExists(root.right, q.val) || isExists(root.left, q.val) && isExists(root.right, p.val)) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        if(left != null) return left;
-        return lowestCommonAncestor(root.right, p, q);
-    }
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p ,q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
+        //if we found someone amoung p and q in right and left the we will return their root
+        if(left != null && right != null) return root;
 
-    public boolean isExists(TreeNode root, int p){
-        if(root == null) return false;
-        if(p == root.val) return true;
-        return isExists(root.left,p) || isExists(root.right,p);
+        //if one of the amoung left and right is null then we will return the non-null element
+        return left != null ? left : right;
     }
 }
