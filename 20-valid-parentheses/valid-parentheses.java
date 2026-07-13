9afaa82375af676bf6d16ab88bcf2617c40f8225
divyanshigupta03 <divyanshigupta1103@gmail.com>
2026-07-14 01:16:10 +0530

    Time: 3 ms (87.00%) | Memory: 43.2 MB (58.17%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 != 0) return false;
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
+            }else{
+                if(st.size() == 0) return false;
+                char top = st.peek();
+                if(sameStyle(top,ch)) st.pop();
+                else return false;
+            }
+        }
+        return st.size()==0;
+    }
+
+    public boolean sameStyle(char a,char b){
+        if(a=='(' && b==')') return true;
+        if(a=='{' && b=='}') return true;
+        if(a=='[' && b==']') return true;
+        return false;
+    }
+}
