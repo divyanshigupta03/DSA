33e5ae56c22297d374d5913807e9419c6efd0592
divyanshigupta03 <divyanshigupta1103@gmail.com>
2026-07-13 17:10:09 +0530

    Time: 1 ms (99.90%) | Memory: 55.6 MB (59.79%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,22 +1,46 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int i=0; i<nums.length; i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
 
-        int max  =  0;
-        for(int i=0; i<nums.length; i++){
-            int count = map.get(nums[i]);
-            max = Math.max(max,count);
-        }
+        // int max  =  0;
+        // for(int i=0; i<nums.length; i++){
+        //     int count = map.get(nums[i]);
+        //     max = Math.max(max,count);
+        // }
 
-        int ans = 0;
+        // int ans = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(map.get(nums[i]) == max){
+        //         ans = nums[i];
+        //     }
+        // }
+        // return ans;
+
+        //MOORE'S ALGORITHM MORE OPTIMIZED
+        int ele = 0;
+        int count = 0;
+        int n = nums.length;
         for(int i=0; i<nums.length; i++){
-            if(map.get(nums[i]) == max){
-                ans = nums[i];
+            if(count == 0){
+                count = 1;
+                ele = nums[i];
+            }else if(nums[i] == ele){
+                count++;
+            }else{
+                count--;
             }
         }
-        return ans;
+
+        //verify
+        count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == ele) count++;
+        }
+
+        if(count > n/2) return ele;
+        return -1;
     }
 }
